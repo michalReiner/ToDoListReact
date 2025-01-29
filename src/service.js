d27577a6a3372a6e9c51999e0207261f0a9d2b2a
michalReiner <michal3213031@gmail.com>
2025-01-29 11:29:38 +0200

    Initial commit

--- src/service.js
+++ src/service.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = process.env.REACT_APP_API_URL;
+const API_BASE_URL = "http://localhost:5062";
 
 async function getTasks() {
     const response = await fetch(`${API_BASE_URL}/items`);
